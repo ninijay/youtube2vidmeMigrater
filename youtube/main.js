c196776f5ddd951c0098503d503e941fa7d12386
Zanidd <zanidd@lootbox420.home>
2017-11-18 23:21:31 +0100

    Implemented auth function

--- youtube/main.js
+++ youtube/main.js
@@ -1,5 +1,28 @@
+var auth;
+
+function getAuth(){
+    $.ajaxSetup({beforeSend: function(xhr){
+        if (xhr.overrideMimeType)
+        {
+          xhr.overrideMimeType("application/json");
+        }
+      }
+      });
+      $.ajaxSetup( { "async": false } );
+   $.getJSON( "youtube/auth.json", function(data) {
+       auth = data;
+    }).fail(function(d, textStatus, error) {
+        console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+    });
+}
+
 function fetchVideos(creator){
     var vids = [];
     // fetchvideos from creator
     return vids;
 }
+
+$(document).ready(function (){
+    getAuth();
+    console.log(auth.key);
+});
