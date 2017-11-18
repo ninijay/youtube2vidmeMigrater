f5a4311825d62f499bbd89560c514ba7eca4c97e
Zanidd <zanidd@lootbox420.home>
2017-11-18 23:49:47 +0100

    Installed Node.js

--- youtube/main.js
+++ youtube/main.js
@@ -24,5 +24,30 @@ function fetchVideos(creator){
 
 $(document).ready(function (){
     getAuth();
-    console.log(auth.key);
 });
+
+
+function keyWordsearch(){
+    gapi.client.setApiKey(auth.key);
+    gapi.client.load('youtube', 'v3', function() {
+            makeRequest();
+    });
+      }
+
+  function makeRequest() {
+    var q = $('#query').val();
+    var request = gapi.client.youtube.search.list({
+            q: q,
+            part: 'snippet', 
+            maxResults: 10
+    });
+    request.execute(function(response)  {                                                                                    
+            $('#results').empty()
+            var srchItems = response.result.items;                      
+            $.each(srchItems, function(index, item) {
+            vidTitle = item.snippet.title;  
+
+            $('#results').append('<pre>' + vidTitle + '</pre>');                      
+    })  
+})  
+}
