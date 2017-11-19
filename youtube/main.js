3e4d6d3ef66d6ad781bf293c2aae5be2240ce940
Zanidd <zanidd@lootbox420.home>
2017-11-19 16:12:07 +0100

    space intendation

--- youtube/main.js
+++ youtube/main.js
@@ -1,38 +1,39 @@
 var auth;
 var vids = [];
 var videoLink = "https://www.youtube.com/watch?v=";
-function getAuth(){
-    $.ajaxSetup({beforeSend: function(xhr){
-        if (xhr.overrideMimeType)
-        {
-          xhr.overrideMimeType("application/json");
+
+function getAuth() {
+    $.ajaxSetup({
+        beforeSend: function (xhr) {
+            if (xhr.overrideMimeType) {
+                xhr.overrideMimeType("application/json");
+            }
         }
-      }
-      });
-      $.ajaxSetup( { "async": false } );
-   $.getJSON( "youtube/auth.json", function(data) {
-       auth = data;
-    }).fail(function(d, textStatus, error) {
-        console.error("getJSON failed, status: " + textStatus + ", error: "+error)
+    });
+    $.ajaxSetup({ "async": false });
+    $.getJSON("youtube/auth.json", function (data) {
+        auth = data;
+    }).fail(function (d, textStatus, error) {
+        console.error("getJSON failed, status: " + textStatus + ", error: " + error)
     });
 }
 
-$(document).ready(function (){
+$(document).ready(function () {
     getAuth();
 });
 
-function getChannelForUser(user){
+function getChannelForUser(user) {
     var user = [];
     gapi.client.setApiKey(auth.key);
-    gapi.client.load('youtube', 'v3', function(){
+    gapi.client.load('youtube', 'v3', function () {
         var request = gapi.client.youtube.channels.list({
-            forUsername : user,
-            part : id,
+            forUsername: user,
+            part: id,
             maxResults: 1
         });
-        request.execute(function(response){
+        request.execute(function (response) {
             var channel = respone.results.items;
-            $.each(channel, function(index, item){
+            $.each(channel, function (index, item) {
                 user.push(item.id);
             })
         })
@@ -40,29 +41,29 @@ function getChannelForUser(user){
     return user[0];
 }
 
-function keyWordsearch(){
+function keyWordsearch() {
     gapi.client.setApiKey(auth.key);
-    gapi.client.load('youtube', 'v3', function() {
-            makeRequest();
+    gapi.client.load('youtube', 'v3', function () {
+        makeRequest();
     });
-      }
+}
 
-  function makeRequest() {
+function makeRequest() {
     var q = $('#query').val();
     var request = gapi.client.youtube.search.list({
-            q: q,
-            part: 'snippet', 
-            maxResults: 100
+        q: q,
+        part: 'snippet',
+        maxResults: 100
     });
-    request.execute(function(response)  {                                                                                    
-            $('#results').empty()
-            var srchItems = response.result.items;                      
-            $.each(srchItems, function(index, item) {
-            vidId = item.id.videoId;  
-            if(typeof vidId != 'undefined'){
-                vids.push(videoLink + vidId );
+    request.execute(function (response) {
+        $('#results').empty()
+        var srchItems = response.result.items;
+        $.each(srchItems, function (index, item) {
+            vidId = item.id.videoId;
+            if (typeof vidId != 'undefined') {
+                vids.push(videoLink + vidId);
             }
-            console.log(videoLink + vidId);                      
-    })  
-})  
+            console.log(videoLink + vidId);
+        })
+    })
 }
