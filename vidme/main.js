b6ee3c0fcf0510d21c756e2790c6dc16f6937379
root <ninijay93@gmail.com>
2017-11-22 18:01:07 +0100

    Initiated vidme functionality

--- vidme/main.js
+++ vidme/main.js
@@ -1,10 +1,30 @@
 var auth;
+var headers;
+var channelId;
+var token_;
+var async_ = false;
 $(document).ready(function () {
     $.getScript("functions/connectivity.js",function()
     {
-       auth =  getAuth("youtube");
+       auth =  getAuth("vidme");
        auth = auth.responseJSON;
        console.log(auth.key);
     });
     
 });
+
+function getToken()
+{
+
+}
+
+function getChannelId(channelName)
+{
+    return false;
+}
+
+function uploadVideo(sourceUrl, vidTitle, vidDescription)
+{
+    isPrivate = false;
+    callLink = "https://api.vid.me/video/upload";
+}
